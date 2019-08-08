17f9263ab3ae3e99810b7a2d2eb7a830bd393c58
Mohammed Abdelrasoul Ismail <mrasoul@modern-electronics.com>
2019-08-09 00:29:14 +0300

    Updates

---
+++ precache-manifest.f6ccbbab7e647d250b83389e82bb83e2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d953dfa82642d2ea6b928680b8960608",
+    "url": "/monsters-rodolex/index.html"
+  },
+  {
+    "revision": "189df54a7e13921ea6ed",
+    "url": "/monsters-rodolex/static/css/main.99b165af.chunk.css"
+  },
+  {
+    "revision": "cea528bd8a6c6ccbefab",
+    "url": "/monsters-rodolex/static/js/2.9918f956.chunk.js"
+  },
+  {
+    "revision": "189df54a7e13921ea6ed",
+    "url": "/monsters-rodolex/static/js/main.bd9728af.chunk.js"
+  },
+  {
+    "revision": "a9260227d0878e8e1bc9",
+    "url": "/monsters-rodolex/static/js/runtime~main.6df6c14a.js"
+  }
+]);
